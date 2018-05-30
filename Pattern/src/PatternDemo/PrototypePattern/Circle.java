1ab944430355a19bccc2ed01b510876f127ab91c
zh-ma <670759275@qq.com>
2018-05-30 16:34:17 +0800

    原型模式

---
+++ Pattern/src/PatternDemo/PrototypePattern/Circle.java
@@ -0,0 +1,11 @@
+package PatternDemo.PrototypePattern;
+
+public class Circle extends Shape{
+    public Circle(){
+        type = "Circle";
+    }
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
